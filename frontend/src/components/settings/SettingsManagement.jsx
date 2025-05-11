2fd2d6cf49b9f8a0fe3560b00cc60c15f4f22e0c
Dan David <d_david8@yahoo.com>
2025-05-11 22:05:44 +0300

    minor fixes

--- frontend/src/components/settings/SettingsManagement.jsx
+++ frontend/src/components/settings/SettingsManagement.jsx
@@ -22,7 +22,7 @@ function WidgetPage () {
         alignItems: 'flex-start',
         gap: 4,
         flexWrap: 'wrap',
-        marginTop: 2, // ensures consistent top spacing
+        marginTop: 2,
       }}
     >
       <WidgetSettings />
@@ -41,7 +41,7 @@ function ChatbotPage () {
         alignItems: 'flex-start',
         gap: 4,
         flexWrap: 'wrap',
-        marginTop: 2, // ensures consistent top spacing
+        marginTop: 2,
       }}
     >
       {/* Left side */}
@@ -79,16 +79,17 @@ function AdvancedSettingsPage () {
           flexWrap: 'wrap',
         }}
       >
-        <Box sx={{flex: 1, minWidth: 300, maxWidth: 550}}>
-          <BotSettings />
-        </Box>
+
         <Box sx={{flex: 1, minWidth: 300, maxWidth: 550}}>
           <OpenAISettings />
+        </Box>
+        <Box sx={{flex: 1, minWidth: 300, maxWidth: 550}}>
+          <BotSettings />
         </Box>
       </Box>
 
       {/* Row 2: Code snippet */}
-      <Box sx={{width: '100%'}}>
+      <Box sx={{maxWidth: 1130}}>
         <CodeSnippet />
       </Box>
     </Box>
@@ -100,6 +101,14 @@ export default function SettingsManagement () {
   const {notification, setNotification} = useStore ();
   const [activeTab, setActiveTab] = useState (0);
   const {logedUser} = useContext (LogedUserContext);
+
+  const tabs = [
+    {label: 'Setări interfață', component: <WidgetPage />},
+    {label: 'Setări chatbot', component: <ChatbotPage />},
+  ];
+  if (logedUser.is_superuser) {
+    tabs.push ({label: 'Setări avansate', component: <AdvancedSettingsPage />});
+  }
 
   const handleTabChange = (event, newValue) => {
     setActiveTab (newValue);
@@ -125,19 +134,16 @@ export default function SettingsManagement () {
         sx={{marginTop: 2, marginBottom: 2}}
         aria-label="Setari tabs"
       >
-        <Tab label="Setări interfață" sx={{textTransform: 'none'}} />
-        <Tab label="Setări chatbot" sx={{textTransform: 'none'}} />
-        <>
-        {logedUser.is_superuser &&
-          <Tab label="Setări avansate" sx={{textTransform: 'none'}} />}
-        </>
+        {tabs.map ((tab, index) => (
+          <Tab key={index} label={tab.label} sx={{textTransform: 'none'}} />
+        ))}
       </Tabs>
 
       {/* Tab content */}
       <Box sx={{marginLeft: 0}}>
         {activeTab === 0 && <WidgetPage />}
         {activeTab === 1 && <ChatbotPage />}
-        {activeTab === 2 && <AdvancedSettingsPage />}
+        {activeTab === 2 && logedUser.is_superuser && <AdvancedSettingsPage />}
       </Box>
 
       {/* Global notification */}
